cd70eb4c3ca834eb09dc34ea1754a6a794c522a8
Franco Contreras <24.f.contreras@gmail.com>
2022-12-17 20:48:54 -0300

    test

--- assets/js/desafio.js
+++ assets/js/desafio.js
@@ -2,11 +2,6 @@ const ratingWindow = document.querySelector("#ratingWindow");
 const thankyouWindow = document.querySelector("#thankyouWindow");
 const ratingForm = document.querySelector("#ratingForm");
 const finalRating = document.querySelector("#finalRating");
-const radioGroup = document.querySelector(".radio-group");
-
-radioGroup.addEventListener("change", (e) => {
-  e.preventDefault;
-});
 
 ratingForm.addEventListener("submit", (e) => {
   e.preventDefault();
